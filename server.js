62ee26cb090a67ae757e02c82a4ba8f05af2a2ef
Mahmoud-Hamza-Git <mahmoudhamza9440@gmail.com>
2023-07-26 18:50:47 +0300

    adding cors

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ MyControllers.startConsumingMessages();
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: '*',
-//   })
-// );
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 app.use(express.static('public'));
 app.use(express.json()); // to accept JSON data from frontend
